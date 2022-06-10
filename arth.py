8e471faed10c9c0990e4f0979dbc9fcb617e4ea6
ndrmohd <mohdndr188@gmail.com>
2022-06-10 10:31:43 +0530

    arithemetic operation

---
+++ arth.py
@@ -0,0 +1,10 @@
+a=int(input('Enter 1st number '))
+b=int(input('Enter 2nd mumber '))
+sum=a+b
+print('sum=',sum)
+difference=a-b
+print('difference=',difference)
+multiplication=a*b
+print('multiplication=',multiplication)
+division=a/b
+print('division=',division)3
