1450c8f7cd1a98df5a7c0429a6a127bf9980f74a
ndrmohd <mohdndr188@gmail.com>
2022-06-11 11:34:54 +0530

    odd or even

---
+++ greater.py
@@ -0,0 +1,9 @@
+a=int(input('Enter the 1st number '))
+b=int(input('Enter the 2nd number '))
+c=int(input('Enter the 3rd number '))
+if a>b and a>c:
+        print(a,'is greater')
+elif b>c:
+    print(b,'is greater')
+else:
+    print(c,'is greater')
